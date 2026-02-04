d917d34a096a546de8989576140ffc30ead6063f
kprajval <spyde40@gmail.com>
2026-02-04 23:56:02 +0530

    TransactionsEnity.java file completed

---
+++ src/main/java/com/neueda/pm_milkyway/entity/TransactionEntity.java
@@ -0,0 +1,75 @@
+package com.neueda.pm_milkyway.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "transactions")
+public class TransactionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private LocalDate date;
+    private String type;
+    private BigDecimal transactionValue;
+    private BigDecimal purseValue;
+
+    private Boolean status;
+
+    public Integer getId() { 
+        return id; 
+    }
+
+    public void setId(Integer id) { 
+        this.id = id; 
+    }
+
+    public LocalDate getDate() { 
+        return date; 
+    }
+
+    public void setDate(LocalDate date) { 
+        this.date = date; 
+    }
+
+    public String getType() { 
+        return type; 
+
+    }
+
+    public void setType(String type) { 
+        this.type = type; 
+    }
+
+    public BigDecimal getTransactionValue() { 
+        return transactionValue; 
+    }
+
+    public void setTransactionValue(BigDecimal transactionValue) { 
+        this.transactionValue = transactionValue; 
+    }
+
+    public BigDecimal getPurseValue() { 
+        return purseValue; 
+    }
+
+    public void setPurseValue(BigDecimal purseValue) { 
+        this.purseValue = purseValue; 
+    }
+
+    public Boolean getStatus() { 
+        return status; 
+    }
+
+    public void setStatus(Boolean status) { 
+        this.status = status; 
+    }
+}
