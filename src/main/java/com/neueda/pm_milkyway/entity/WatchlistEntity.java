43168b3b18a264ff017a129fdbe786fcfb1fae5f
kprajval <spyde40@gmail.com>
2026-02-04 23:56:26 +0530

    WatchlistEnity.java file completed

---
+++ src/main/java/com/neueda/pm_milkyway/entity/WatchlistEntity.java
@@ -0,0 +1,34 @@
+package com.neueda.pm_milkyway.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "watchlist")
+public class WatchlistEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    private String stock;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+}
