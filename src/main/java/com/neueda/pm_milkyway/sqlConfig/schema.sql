21e7f787b1fe0872bcbac49008a0c730db05f178
kprajval <spyde40@gmail.com>
2026-02-04 23:35:17 +0530

    mysql schema defined and created on mysql server

--- src/main/java/com/neueda/pm_milkyway/sqlConfig/schema.sql
+++ src/main/java/com/neueda/pm_milkyway/sqlConfig/schema.sql
@@ -22,3 +22,15 @@ create table if not exists holdings(
     quantity int not null,
     total_invested decimal(15,2) not null
 );
+
+insert into watchlist (stock) values ('AAPL'), ('GOOGL'), ('MSFT');
+
+insert into transactions (date, type, transaction_value, purse_value, status) values
+('2026-01-30', 'PURSE ADD', 10000, 10000, true),
+('2026-02-02', 'BUY AAPL', 2700, 7300, true),
+('2026-02-03', 'BUY GOOGL', 3400, 3900, true),
+('2026-02-03', 'BUY MSFT', 4100, 3900, false);
+
+insert into holdings (stock, quantity, total_invested) values
+('AAPL', 10, 2700),
+('GOOGL', 10, 3400);
