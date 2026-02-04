876ccd46f8b94023bb5cff1f0f14a519f594da0b
KURUMETI PRAJVAL <113627202+kprajval@users.noreply.github.com>
2026-02-04 23:01:50 +0530

    Delete portfolio-manager directory

--- portfolio-manager/src/main/java/com/portfolio/portfolio_manager/repo/PortfolioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.portfolio.portfolio_manager.repo;
-
-import com.portfolio.portfolio_manager.entity.Portfolio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
-}
