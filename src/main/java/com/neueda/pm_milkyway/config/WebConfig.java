8e7b4248e0cb9c5fb65a1296356bca9f34e0906c
abc <ranjeet.workuse@gmail.com>
2026-02-06 05:26:13 +0000

    Live Server

---
+++ src/main/java/com/neueda/pm_milkyway/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.neueda.pm_milkyway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // Use allowedOriginPatterns instead of allowedOrigins for flexibility with
+                                            // credentials
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
